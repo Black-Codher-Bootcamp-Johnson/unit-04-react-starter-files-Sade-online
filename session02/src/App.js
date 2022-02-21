97dead1461e00d30da5d60b7ea0de2e48d4feeb8
sade-online <fot.fasan@gmail.com>
2022-02-21 21:09:36 +0000

    latest updates

---
+++ session02/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>My React Page</h1>
+      <p>This is my first React page</p>
+    </div>
+  );
+}
+
+const h1Style = {
+  "fontSize":"30px",
+  "color": "#cc0000",
+  "textTransform": "uppercase"
+ };
+
+export default App;
