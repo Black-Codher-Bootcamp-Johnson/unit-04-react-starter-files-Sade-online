f3249b3b8882d3f15b51b8233b5d11175a203b0c
sade-online <fot.fasan@gmail.com>
2022-03-07 22:08:28 +0000

    start of session 7

--- session07/src/index.js
+++ session07/src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom';
 
 const LogForm = () => {
   const [name, setName] = useState('');
+  const [age, setAge] = useState (0);
+  const [location, setLocation] = useState('');
+
   const [,setSubmitted] = useState(false);
 
   function handleSubmit(event) {
@@ -17,14 +20,25 @@ const LogForm = () => {
       <form onSubmit={(e) => handleSubmit(e)}>
         <div>
           <label>Name:<input type="text" value={name} onChange={(e) => { setName(e.target.value); setSubmitted(false) }} /></label>
+
+          <label>Age:<input type="number" value={age} onChange={(e) => { setAge(e.target.value); setSubmitted(false) }} /></label>
+
+          <label>Location:<input type="text" value={location} onChange={(e) => { setLocation(e.target.value); setSubmitted(false) }} /></label>
+
+
         </div>
         <input type="submit" value="Submit" />
       </form>
+
+
       <div>
         <h2>Results</h2>
             Name: {name}<br />
+            Age: {age}<br />
+            Location: {location}< br />
       </div>
     </div>
+
   );
 }
 
